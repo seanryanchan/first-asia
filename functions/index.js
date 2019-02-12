0a89037bd604ca8ea244c19f6b98ba7d04604db2
Sean Ryan Chan <seanryachan@gmail.com>
2019-02-12 18:50:15 +0800

    some fixes

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,18 @@ const functions = require('firebase-functions');
 const express = require('express');
 const firstAsiaApp = express();
 
+firstAsiaApp.get('/about', (request,response) => {
+  response.sendFile('../public/about.html');
+  next();
+});
+firstAsiaApp.get('/products', (request,response) => {
+  response.sendFile('../public/products.html');
+  next();
+});
+
 firstAsiaApp.get('/', (request,response) => {
   response.sendFile('../public/index.html');
+  next();
 });
 
 firstAsiaApp.use(express.static('../public'));
